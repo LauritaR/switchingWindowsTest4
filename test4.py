ab99d0dee15583ad1accec4573f24348df85b5ee
Laurita R <laurita.radisevska@mif.stud.vu.lt>
2025-04-03 11:04:17 +0300

    added libs

--- test4.py
+++ test4.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time 
 import pytest
 
 
@@ -14,21 +18,28 @@ class TestRegistration:
     
     def fill_registration_form(self, registration_url):
         self.driver.get(registration_url)
-        self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[1]/input').send_keys('Robotas')
-        self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[2]/input').send_keys('Robotauskas')
-        self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[3]/input').send_keys('robotas.robotauskas@mif.stud.vu.lt')
-        self.driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[1]/input').send_keys('+37061234567')
-        self.driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[2]/input').send_keys('Didlaukio g. 59, Vilnius')
-        self.driver.find_element(By.XPATH, '/html/body/div/form/button').click()
-        self.driver.implicitly_wait(5)
-
+        try:
+            self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[1]/input').send_keys('Robotas')
+            self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[2]/input').send_keys('Robotauskas')
+            self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[3]/input').send_keys('robotas.robotauskas@mif.stud.vu.lt')
+            self.driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[1]/input').send_keys('+37061234567')
+            self.driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[2]/input').send_keys('Didlaukio g. 59, Vilnius')
+            self.driver.find_element(By.XPATH, '/html/body/div/form/button').click()
+        except NoSuchElementException:
+            return False
+        return True
+    
     def test_registration1(self):
         self.fill_registration_form("http://suninjuly.github.io/registration1.html")
-        success_text = self.driver.find_element(By.TAG_NAME, 'h1').text
+        success_text = WebDriverWait(self.driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
     
     def test_registration2(self):
         self.fill_registration_form("http://suninjuly.github.io/registration2.html")
-        success_text = self.driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(self.driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
     
--- test5.py
+++ test5.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_and_teardown():
@@ -33,61 +36,81 @@ def setup_and_teardown_session():
 
 def fill_registration_form(driver, registration_url):
     driver.get(registration_url)
-    driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[1]/input').send_keys('Robotas')
-    driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[2]/input').send_keys('Robotauskas')
-    driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[3]/input').send_keys('robotas.robotauskas@mif.stud.vu.lt')
-    driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[1]/input').send_keys('+37061234567')
-    driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[2]/input').send_keys('Didlaukio g. 59, Vilnius')
-    driver.find_element(By.XPATH, '/html/body/div/form/button').click()
+    try:
+        driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[1]/input').send_keys('Robotas')
+        driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[2]/input').send_keys('Robotauskas')
+        driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[3]/input').send_keys('robotas.robotauskas@mif.stud.vu.lt')
+        driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[1]/input').send_keys('+37061234567')
+        driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[2]/input').send_keys('Didlaukio g. 59, Vilnius')
+        driver.find_element(By.XPATH, '/html/body/div/form/button').click()
+    except NoSuchElementException:
+            return False
+    return True
 
 class TestRegistrationFunction:
     def test_registration1(self, setup_and_teardown):
         driver = setup_and_teardown
         fill_registration_form(driver, "http://suninjuly.github.io/registration1.html")
-        success_text = driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
 
     def test_registration2(self, setup_and_teardown):
         driver = setup_and_teardown
         fill_registration_form(driver, "http://suninjuly.github.io/registration2.html")
-        success_text = driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
 
 class TestRegistrationClass:
     def test_registration1(self, setup_and_teardown_class):
         driver = setup_and_teardown_class
         fill_registration_form(driver, "http://suninjuly.github.io/registration1.html")
-        success_text = driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
 
     def test_registration2(self, setup_and_teardown_class):
         driver = setup_and_teardown_class
         fill_registration_form(driver, "http://suninjuly.github.io/registration2.html")
-        success_text = driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
 
 class TestRegistrationModule:
     def test_registration1(self, setup_and_teardown_module):
         driver = setup_and_teardown_module
         fill_registration_form(driver, "http://suninjuly.github.io/registration1.html")
-        success_text = driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
 
     def test_registration2(self, setup_and_teardown_module):
         driver = setup_and_teardown_module
         fill_registration_form(driver, "http://suninjuly.github.io/registration2.html")
-        success_text = driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
 
 class TestRegistrationSession:
     def test_registration1(self, setup_and_teardown_session):
         driver = setup_and_teardown_session
         fill_registration_form(driver, "http://suninjuly.github.io/registration1.html")
-        success_text = driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
 
     def test_registration2(self, setup_and_teardown_session):
         driver = setup_and_teardown_session
         fill_registration_form(driver, "http://suninjuly.github.io/registration2.html")
-        success_text = driver.find_element(By.TAG_NAME, 'h1').text
+        success_text =  WebDriverWait(driver,5).until(
+            EC.presence_of_element_located((By.TAG_NAME,'h1'))
+        ).text
         assert success_text == 'Congratulations! You have successfully registered!'
